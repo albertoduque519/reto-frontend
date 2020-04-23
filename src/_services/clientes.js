baf60ba7cc4b06ba05780b94192a9a257c7eba29
alberto Duque <desarrollo.tinger@gmail.com>
2020-04-22 21:18:14 -0500

    actualizar plantilla

--- src/_services/clientes.js
+++ src/_services/clientes.js
@@ -5,7 +5,8 @@ import querystring from 'querystring'
 
 export const clientService = {
   getClients,
-  getDiskSpaceStatsByClient
+  getDiskSpaceStatsByClient,
+  getDiskSpaceStatsByDate
 }
 
 async function getClients() {
@@ -15,4 +16,9 @@ async function getClients() {
 async function getDiskSpaceStatsByClient(idClientes) {
   let queryParams = { 'idClientes': idClientes }
   return axios.get(`${process.env.VUE_APP_URLAPI_AUTHENTICATION}/stats/client-disk-usage?`, authHeader(queryParams))
+}
+
+async function getDiskSpaceStatsByDate(idClientes, startDate, endDate) {
+  let queryParams = { 'idClientes': idClientes, 'startDate': startDate, 'endDate': endDate }
+  return axios.get(`${process.env.VUE_APP_URLAPI_AUTHENTICATION}/stats/client-disk-usage-date?`, authHeader(queryParams))
 }
